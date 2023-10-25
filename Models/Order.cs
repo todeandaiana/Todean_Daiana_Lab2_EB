babcc18a9d31473fa722082eb6bb8edcdaae5872
Daiana <Daiana@DESKTOP-4N9BAGI>
2023-10-25 19:54:46 +0300

    .

--- Models/Order.cs
+++ Models/Order.cs
@@ -12,6 +12,7 @@ namespace Todean_Daiana_Lab2.Models
         public int OrderID { get; set; }
         public int CostumerID { get; set; }
         public int BookID {  get; set; }
+        public DateTime? OrderDate {  get; set; }
 
         public Customer Customer { get; set; }
         public Book Book { get; set; }
